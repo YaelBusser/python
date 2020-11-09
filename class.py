dae8ee8b9e06fd7db8b1a9a0cb5546f63910f557
= <=>
2020-11-09 11:54:12 +0100

    Ajout exo

--- class.py
+++ class.py
@@ -17,15 +17,29 @@
 # if __name__ == '__main__' :
 #     a = Carre(10)
 #     print("Le carré à un côté d'une longueur de " + str(a.unCote) + ", une aire de " + str(a.aire) + " et un périmètre de " + str(a.perimetre) + ".")
-class Carre:
-    def __init__(self, cote):
-        self.unCote = cote
-        self.perimetre = self.fctPerimetre() 
-        self.aire = self.unCote * self.unCote
-    def fctPerimetre(self):                  
-        return self.unCote * 4         
 
-if __name__ == '__main__' :
-    a = Carre(10) 
-    b = Carre(5)
-    print(a.aire + b.aire)
+# class Carre:
+#     def __init__(self, cote):
+#         self.unCote = cote
+#         self.perimetre = self.fctPerimetre() 
+#         self.aire = self.unCote * self.unCote
+#     def fctPerimetre(self):                  
+#         return self.unCote * 4         
+
+# if __name__ == '__main__' :
+#     a = Carre(10) 
+#     b = Carre(5)
+#     print(a.aire + b.aire)
+
+# class Carre:
+#     def __init__(self, cote):
+#         self.unCote = cote
+#         self.perimetre = self.fctPerimetre() 
+#         self.aire = self.unCote * self.unCote
+#     def fctPerimetre(self):                  
+#         return self.unCote * 4         
+
+# if __name__ == '__main__' :
+#     a = Carre(10) 
+#     b = Carre(5)
+#     print(a.aire - b.aire)
