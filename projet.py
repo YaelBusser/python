4dc154ad80b40096ea43f357cd5ee1e2c2a774cd
= <=>
2020-11-09 15:42:23 +0100

    Ajout class

--- projet.py
+++ projet.py
@@ -1,7 +1,19 @@
+class Client: 
+    def __init__(self, nom, prenom, voiture):
+        self.nomClient = nom
+        self.prenomClient = prenom
+        self.voitureClient = voiture
 class Voiture:
-    def __init__(self, marque, couleur):
-        self.laMarque = marque
-        self.laCouleur =  couleur
+    def __init__(self, nom, prix, marque, couleur, portes, roues, places, manuelle, auto):
+       self.nomVoiture = nom
+       self.prixVoiture = prix
+       self.marqueVoiture = marque
+       self.couleurVoiture = couleur
+       self.portesVoiture = portes
+       self.rouesVoiture = roues
+       self.placesVoiture = places
+       self.manuelleVoiture = manuelle
+       self.autoVoiture = auto
 if __name__ == "__main__":
-
-        print("ok")
+    voitureClient = Voiture("Shelby GT 500 Eleanor 1967", 100000, "Ford Mustang", "Noir", 3, 4, 2, True, False)
+    print(voitureClient.prixVoiture)
