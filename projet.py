deb22f9b475dbdb5b5051846bf98a5bdf8da7a38
= <=>
2020-11-09 14:58:45 +0100

    Ajoute fichier projet.py

---
+++ projet.py
@@ -0,0 +1,11 @@
+class Voiture:
+    def __init__(self, marque, couleur):
+        self.laMarque = marque
+        self.laCouleur =  couleur
+if __name__ == "__main__":
+    laMarque = input("Quelle est la marque ? ")
+    laCouleur = input("Quelle est la couleur ? ")
+    tabMarque = ("Peugeot", "Renault", "Opel", "Citroën", "Volkswagen", "Mercedes", "BMW", "Fiat", "Audi", "Ford", "Nissan", "Toyota")
+    tabCouleur = ("Jaune","Orange","Blanc","Noire","Rouge","Rose,","Violet","Vert","Marron","Bleu", "Gris")
+    if laMarque in tabMarque and laCouleur in tabCouleur:
+        print("ok")
