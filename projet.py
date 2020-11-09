71f3d9da94ca96bef4836d31c578d23c7b395fbc
= <=>
2020-11-09 20:02:24 +0100

    jcps

--- projet.py
+++ projet.py
@@ -1,19 +1,28 @@
 class Client: 
-    def __init__(self, nom, prenom, voiture):
+    def __init__(self, identifiant, nom, prenom, voiture):
+        self.idClient = identifiant
         self.nomClient = nom
         self.prenomClient = prenom
         self.voitureClient = voiture
+
 class Voiture:
+    identifiantVoiture = 0
     def __init__(self, nom, prix, marque, couleur, portes, roues, places, manuelle, auto):
-       self.nomVoiture = nom
-       self.prixVoiture = prix
-       self.marqueVoiture = marque
-       self.couleurVoiture = couleur
-       self.portesVoiture = portes
-       self.rouesVoiture = roues
-       self.placesVoiture = places
-       self.manuelleVoiture = manuelle
-       self.autoVoiture = auto
+        self.idVoiture = type(self).identifiantVoiture
+        type(self).identifiantVoiture += 1
+        self.nomVoiture = nom
+        self.prixVoiture = prix
+        self.marqueVoiture = marque
+        self.couleurVoiture = couleur
+        self.portesVoiture = portes
+        self.rouesVoiture = roues
+        self.placesVoiture = places
+        self.manuelleVoiture = manuelle
+        self.autoVoiture = auto
 if __name__ == "__main__":
-    voitureClient = Voiture("Shelby GT 500 Eleanor 1967", 100000, "Ford Mustang", "Noir", 3, 4, 2, True, False)
-    print(voitureClient.prixVoiture)
+
+    Moi = Voiture("Shelby GT 500 Eleanor 1967", 100000, "Ford Mustang", "Noir", 3, 4, 2, True, False)
+    Toi = Voiture("jcps", 100000, "Twingo", "Vert", 3, 4, 2, True, False)
+    
+    print(Moi.idVoiture)
+    print(Toi.idVoiture)
