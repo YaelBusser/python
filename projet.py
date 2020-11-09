f74b15df5e1bebdb764e8acbe875082fc1e8e0db
= <=>
2020-11-09 15:05:41 +0100

    Ajoute fichier projet.py

--- projet.py
+++ projet.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     laMarque = input("Quelle est la marque ? ")
     laCouleur = input("Quelle est la couleur ? ")
     tabMarque = ("Peugeot", "Renault", "Opel", "Citroën", "Volkswagen", "Mercedes", "BMW", "Fiat", "Audi", "Ford", "Nissan", "Toyota")
-    tabCouleur = ("Jaune","Orange","Blanc","Noire","Rouge","Rose,","Violet","Vert","Marron","Bleu", "Gris")
+    tabCouleur = ("Jaune","Orange","Blanc","Noire","Rouge","Rose","Violet","Vert","Marron","Bleu", "Gris")
     if laMarque in tabMarque and laCouleur in tabCouleur:
         print("ok")
