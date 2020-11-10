150729cac9ba47c9af1235322c818348b3176d7f
= <=>
2020-11-10 09:31:19 +0100

    Ajout fichier projet et séparation class

--- projet/voiture.py
+++ projet/voiture.py
@@ -1,6 +1,6 @@
 class Voiture:
     identifiantVoiture = 0
-    def __init__(self, nom, prix, marque, couleur, portes, roues, places, manuelle, auto):
+    def __init__(self, nom, prix, marque, couleur, portes, places, manuelle, auto):
         self.idVoiture = type(self).identifiantVoiture #ou Voiture.identificationVoiture
         type(self).identifiantVoiture += 1
         self.nomVoiture = nom
@@ -8,14 +8,15 @@ class Voiture:
         self.marqueVoiture = marque
         self.couleurVoiture = couleur
         self.portesVoiture = portes
-        self.rouesVoiture = roues
         self.placesVoiture = places
         self.manuelleVoiture = manuelle
         self.autoVoiture = auto
 if __name__ == "__main__":
 
-    Moi = Voiture("Shelby GT 500 Eleanor 1967", 100000, "Ford Mustang", "Noir", 3, 4, 2, True, False)
-    Toi = Voiture("jcps", 100000, "Twingo", "Vert", 3, 4, 2, True, False)
+    Client1 = Voiture("Shelby GT 500 Eleanor 1967", 100000, "Ford Mustang", "Noir", 3, 2, True, False)
+    Client2 = Voiture("Renault Twingo 3 GT", 1000, "Twingo", "Vert", 3, 4, True, False)
+    Client3 = Voiture("Bugatti Veyron", 1000000, "Bugatti", "Noir", 3, 2, True, False)
     
-    print(Moi.idVoiture)
-    print(Toi.idVoiture)
+    print(Client1.idVoiture)
+    print(Client2.idVoiture)
+    print(Client3.idVoiture)
