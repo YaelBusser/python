208fa8f964dfa1dcd6a6c1354cf2cc65d74acd5a
= <=>
2020-11-10 09:56:44 +0100

    Modification et importation de la class voiture dans client.py et ajout fichier objectifs.txt

--- projet/client.py
+++ projet/client.py
@@ -1,8 +1,18 @@
+import voiture
 class Client: 
-    def __init__(self, nom, prenom, voiture):
+    def __init__(self, nom, prenom, idVoiture):
         self.nomClient = nom
         self.prenomClient = prenom
-        self.voitureClient = voiture
+        self.idVoitureClient = idVoiture
 
 if __name__ == "__main__":
+    voiture1 = voiture.Voiture("Shelby GT 500 Eleanor 1967", 100000, "Ford Mustang", "Noir", 3, 2, True, False)
+    client1 = Client("Busser", "Yaël", voiture1.idVoiture)
+
+    voiture2 = voiture.Voiture("Bugatti Veyron", 1000000, "Bugatti", "Noir", 3, 2, True, False)
+    client2 = Client("Nicola", "Civile", voiture2.idVoiture)
     
+    print(client1.idVoitureClient)
+    print(voiture1.idVoiture)
+    print(client2.idVoitureClient)
+    print(voiture2.idVoiture)
--- projet/voiture.py
+++ projet/voiture.py
@@ -11,12 +11,12 @@ class Voiture:
         self.placesVoiture = places
         self.manuelleVoiture = manuelle
         self.autoVoiture = auto
-if __name__ == "__main__":
 
-    Client1 = Voiture("Shelby GT 500 Eleanor 1967", 100000, "Ford Mustang", "Noir", 3, 2, True, False)
-    Client2 = Voiture("Renault Twingo 3 GT", 1000, "Twingo", "Vert", 3, 4, True, False)
-    Client3 = Voiture("Bugatti Veyron", 1000000, "Bugatti", "Noir", 3, 2, True, False)
+if __name__ == "__main__":
+    voiture1 = Voiture("Shelby GT 500 Eleanor 1967", 100000, "Ford Mustang", "Noir", 3, 2, True, False)
+    voiture2 = Voiture("Renault Twingo 3 GT", 1000, "Twingo", "Vert", 3, 4, True, False)
+    voiture3 = Voiture("Bugatti Veyron", 1000000, "Bugatti", "Noir", 3, 2, True, False)
     
-    print(Client1.idVoiture)
-    print(Client2.idVoiture)
-    print(Client3.idVoiture)
+    print(voiture1.idVoiture)
+    print(voiture2.idVoiture)
+    print(voiture3.idVoiture)
